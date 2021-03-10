e1c6115c5cefcdd499fa8787c7faac678cbda7c9
Henrikas Tepelis <P2447304@my365.dmu.ac.uk>
2021-03-10 20:14:34 +0000

    started code on testing customer

---
+++ ClassLibrary/clsCustomer.cs
@@ -0,0 +1,7 @@
+﻿namespace ClassLibrary
+{
+    public class clsCustomer
+    {
+        public int CustomerId { get; set; }
+    }
+}
---
+++ Testing1/TstCustomer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class
+            clsCustomer ACustomer = new clsCustomer();
+            //test to see that it exists
+            Assert.IsNotNull(ACustomer);
+        }
+    }
+
+    [TestMethod]
+    public void CustomerIdOK()
+    {
+        //
+        clsCustomer ACustomer = new clsCustomer();
+        int TestData = 2;
+        ACustomer.CustomerId = TestData;
+        //
+        Assert.AreEqual(ACustomer.CustomerId,TestData);
+    }
+}
