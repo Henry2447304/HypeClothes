885e3121f785e7d46583b7d8cac2d233f81459b1
Henrikas Tepelis <P2447304@my365.dmu.ac.uk>
2021-03-10 21:22:44 +0000

    added more tests from example video

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -1,7 +1,14 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsCustomer
     {
         public int CustomerId { get; set; }
+
+        public string Valid(int mCustomerId)
+        {
+            return "";
+        }
     }
 }
--- Testing1/TstCustomer.cs
+++ Testing1/TstCustomer.cs
@@ -7,6 +7,8 @@ namespace Testing1
     [TestClass]
     public class tstCustomer
     {
+        //good test data
+        private int mCustomerId = 2;
         [TestMethod]
         public void InstanceOK()
         {
@@ -15,16 +17,37 @@ namespace Testing1
             //test to see that it exists
             Assert.IsNotNull(ACustomer);
         }
-    }
 
-    [TestMethod]
-    public void CustomerIdOK()
-    {
-        //
-        clsCustomer ACustomer = new clsCustomer();
-        int TestData = 2;
-        ACustomer.CustomerId = TestData;
-        //
-        Assert.AreEqual(ACustomer.CustomerId,TestData);
+        [TestMethod]
+        public void CustomerIdOK()
+        {
+           
+            clsCustomer ACustomer = new clsCustomer();
+            int TestData = mCustomerId;
+            ACustomer.CustomerId = TestData;
+            Assert.AreEqual(ACustomer.CustomerId, TestData);
+        }
+
+        [TestMethod]
+        public void ValidOK()
+        {
+            
+            clsCustomer ACustomer = new clsCustomer();
+            string Error = "";
+            int TestData = mCustomerId;
+            Error = ACustomer.Valid(mCustomerId);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void MinLessOne()
+        {
+
+            clsCustomer ACustomer = new clsCustomer();
+            string Error = "";
+            int TestData = 0;
+            Error = ACustomer.Valid(TestData);
+            Assert.AreNotEqual(Error, "");
+        }
     }
 }
