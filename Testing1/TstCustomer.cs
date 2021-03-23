63c8e3c08555636544a25462d6f38611aa64e3c7
Henrikas Tepelis <P2447304@my365.dmu.ac.uk>
2021-03-23 03:58:44 +0000

    added from henry branch to master

--- Testing1/TstCustomer.cs
+++ Testing1/TstCustomer.cs
@@ -7,6 +7,8 @@ namespace Testing1
     [TestClass]
     public class tstCustomer
     {
+        //good test data
+        private int mCustomerId = 2;
         [TestMethod]
         public void InstanceOK()
         {
@@ -19,14 +21,33 @@ namespace Testing1
         [TestMethod]
         public void CustomerIdOK()
         {
-            //
+
             clsCustomer ACustomer = new clsCustomer();
-            int TestData = 2;
+            int TestData = mCustomerId;
             ACustomer.CustomerId = TestData;
-            //
             Assert.AreEqual(ACustomer.CustomerId, TestData);
         }
-    }
 
-    
+        [TestMethod]
+        public void ValidOK()
+        {
+
+            clsCustomer ACustomer = new clsCustomer();
+            string Error = "";
+            int TestData = mCustomerId;
+            Error = ACustomer.Valid(mCustomerId);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void MinLessOne()
+        {
+
+            clsCustomer ACustomer = new clsCustomer();
+            string Error = "";
+            int TestData = 0;
+            Error = ACustomer.Valid(TestData);
+            Assert.AreNotEqual(Error, "");
+        }
+    }
 }
