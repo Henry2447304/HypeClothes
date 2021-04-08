0d2f0d0c1a84008757a78cab68f45d4404d5326f
Leo <P2645538@my365.dmu.ac.uk>
2021-04-08 11:34:43 +0100

    Added test for working delete function and created a confirm delete page

--- AdminSystem/OrderConfirmDelete.aspx.cs
+++ AdminSystem/OrderConfirmDelete.aspx.cs
@@ -1,14 +1,31 @@
-﻿using System;
+﻿using ClassLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class _1_ConfirmDelete : System.Web.UI.Page
+public partial class DeleteOrder : System.Web.UI.Page
 {
+    //var to store the primary key value of the record to be deleted
+    Int32 OrderID;
+    //event handler for the load event
     protected void Page_Load(object sender, EventArgs e)
     {
+        //get the number of the address to be deleted from the session object
+        OrderID = Convert.ToInt32(Session["OrderID"]);
+    }
 
+    protected void btnYes_Click(object sender, EventArgs e)
+    {
+        //create a new instance of the order list
+        clsOrderCollection OrderList = new clsOrderCollection();
+        //find the record to delete
+        OrderList.ThisOrder.Find(OrderID);
+        //delete the record
+        OrderList.Delete();
+        //redirect back to the main page
+        Response.Redirect("OrderList.aspx");
     }
 }
