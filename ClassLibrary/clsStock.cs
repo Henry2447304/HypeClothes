820dff89e69bc6a96fd536237c5a805155c4f7f7
Neil Mistry <neilmistry@hotmail.co.uk>
2021-05-19 22:57:36 +0100

    test and class changes made

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -4,11 +4,108 @@ namespace ClassLibrary
 {
     public class clsStock
     {
-        public int ProductNo { get; set; }
-        public int Cost { get; set; }
-        public string ProductDescript { get; set; }
-        public int StockNo { get; set; }
-        public DateTime DateAdded { get; set; }
-        public bool InStck { get; set; }
+        //private data member for the product ID
+        private Int32 mProductID;
+        public Int32 ProductNo
+        {
+            get
+            {
+                return mProductID;
+            }
+            set
+            {
+                mProductID = value;
+            }
+        }
+
+        private int mCost;
+        //product cost public property
+        public int Cost
+        {
+            get
+            {
+                return mCost;
+            }
+            set
+            {
+                mCost = value;
+            }
+        }
+
+        //product description private member variable
+        private string mProductDescript;
+        //Product description public property
+        public string ProductDescript
+        {
+            get
+            {
+                return mProductDescript;
+            }
+            set
+            {
+                mProductDescript = value;
+            }
+        }
+
+
+        //stockNo private member variable
+        private Int32 mStockNo;
+        //StockNo public property
+        public int StockNo
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mStockNo;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mStockNo = value;
+            }
+        }
+
+        public DateTime DateAdded
+        {
+            get
+            {
+                return mDateAdded;
+            }
+            set
+            {
+                mDateAdded = value;
+            }
+        }
+
+        //private data member for active
+        private Boolean mInStck;
+        //public property for active
+        public bool InStck
+        {
+            get
+            {
+                //return the private data
+                return mInStck;
+            }
+            set
+            {
+                //set the private data
+                mInStck = value;
+            }
+        }
+
+
+        public bool Find(int stockNo)
+        {
+            //set the private data members to the test data value
+            mProductID = 2;
+            mDateAdded = Convert.ToDateTime("10/02/2021");
+            mProductDescript = "Black NOCTA Hoodie";
+            mCost = 120;
+            mStockNo = 32;
+            mInStck = true;
+            //always return true
+            return true;
+        }
     }
 }
--- Testing5/tstStock.cs
+++ Testing5/tstStock.cs
@@ -93,5 +93,150 @@ namespace Testing5
             //test to see that the two values are the same
             Assert.AreEqual(AnStock.InStck, TestData);
         }
+
+        [TestMethod]
+        public void FindMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsStock AnStock = new clsStock();
+            //Boolean variable to store the results of the validation
+            Boolean Found = false;
+            //create some test data to use with the method
+            Int32 StockNo = 32;
+            //invoke the method
+            Found = AnStock.Find(StockNo);
+            //test to see if the result is true
+            Assert.IsTrue(Found);
+        }
+
+        [TestMethod]
+        public void TestProductNoFound()
+        {
+            //create an instance of the class we want to create
+            clsStock AnStock = new clsStock();
+            //boolean variable to record if data is OK
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 ProductNo = 2;
+            Found = AnStock.Find(ProductNo);
+            if (AnStock.ProductNo != 2)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestDateAddedFound()
+        {
+            //create an instance of the class we want to create
+            clsStock AnStock = new clsStock();
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 ProductNo = 2;
+            //invoke the method
+            Found = AnStock.Find(ProductNo);
+            //check
+            if (AnStock.DateAdded != Convert.ToDateTime("16/09/2021"))
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestStockNoFound()
+        {
+            //create an instance of the class we want to create
+            clsStock AnStock = new clsStock();
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method 
+            Int32 StockNo = 50;
+            //invoke the method
+            Found = AnStock.Find(StockNo);
+            //check the address no
+            if (AnStock.StockNo != 50)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestCostFound()
+        {
+            //create an instance of the class we want to create
+            clsStock AnStock = new clsStock();
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            int Cost = 120;
+            //invoke the method
+            Found = AnStock.Find(Cost);
+            //check the property
+            if (AnStock.Cost != 120)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestProductDescriptionFound()
+        {
+            //create an instance of the class we want to create
+            clsStock AnStock = new clsStock();
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            string ProductDescript = "Black NOCTA Hoodie";
+            //invoke the method
+            Found = AnStock.Find(ProductDescript);
+            //check the property
+            if (AnStock.ProductDescript != "Black NOCTA HoodieL")
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestInStockFound()
+        {
+            //create an instance of the class we want to create
+            clsStock AnStock = new clsStock();
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 StockNo = 50;
+            //invoke the method
+            Found = AnStock.Find(StockNo);
+            //check the property
+            if (AnStock.InStck != true)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+
     }
 }
