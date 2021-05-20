8a7373759c82945ca86a35e66508e9fbf1cd9282
Henrikas Tepelis <P2447304@my365.dmu.ac.uk>
2021-05-20 23:04:21 +0100

    collection class

---
+++ ClassLibrary/clsCustomerCollection.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+
+    public class clsCustomerCollection
+    {
+
+
+        public clsCustomerCollection()
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.Execute("sproc_TblCustomer_SelectAll");
+            PopulateArray(DB);
+
+        }
+        List<clsCustomer> mCustomerList = new List<clsCustomer>();
+        public List<clsCustomer> CustomerList
+        {
+            get
+            {
+                return mCustomerList;
+            }
+            set
+            {
+                mCustomerList = value;
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return mCustomerList.Count;
+            }
+            set
+            {
+               
+            }
+        }
+        clsCustomer mThisCustomer = new clsCustomer();
+        public clsCustomer ThisCustomer
+        {
+            get
+            {
+                return mThisCustomer;
+            }
+            set
+            {
+                mThisCustomer = value;
+            }
+
+        }
+
+        public int Add()
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("CustomerName", mThisCustomer.Name);
+            DB.AddParameter("Address", mThisCustomer.Address);
+            DB.AddParameter("Postcode", mThisCustomer.Postcode);
+            DB.AddParameter("DoB", mThisCustomer.DoB);
+            DB.AddParameter("GdprRequest", mThisCustomer.GdprRequest);
+            return DB.Execute("sproc_TblCustomer_Add");
+        }
+        public void Update()
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("CustomerId", mThisCustomer.CustomerId);
+            DB.AddParameter("CustomerName", mThisCustomer.Name);
+            DB.AddParameter("Address", mThisCustomer.Address);
+            DB.AddParameter("Postcode", mThisCustomer.Postcode);
+            DB.AddParameter("DoB", mThisCustomer.DoB);
+            DB.AddParameter("GdprRequest", mThisCustomer.GdprRequest);
+            DB.Execute("sproc_TblCustomer_Update");
+        }
+
+        public void Delete()
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@CustomerId", mThisCustomer.CustomerId);
+            DB.Execute("sproc_tblCustomer_Delete");
+        }
+
+
+        public void ReportByName(string Name)
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@CustomerName", Name);
+            DB.Execute("sproc_TblCustomer_FilterByName");
+            PopulateArray(DB);
+        }
+
+        void PopulateArray(clsDataConnection DB)
+        {
+            Int32 Index = 0;
+            Int32 NameCount;
+            NameCount = DB.Count;
+            mCustomerList = new List<clsCustomer>();
+            while (Index < NameCount)
+            {
+                clsCustomer ACustomer = new clsCustomer();
+                ACustomer.CustomerId = Convert.ToInt32(DB.DataTable.Rows[Index]["CustomerId"]);
+                ACustomer.Name = Convert.ToString(DB.DataTable.Rows[Index]["CustomerName"]);
+                ACustomer.Address = Convert.ToString(DB.DataTable.Rows[Index]["Address"]);
+                ACustomer.Postcode = Convert.ToString(DB.DataTable.Rows[Index]["Postcode"]);
+                ACustomer.DoB = Convert.ToDateTime(DB.DataTable.Rows[Index]["DoB"]);
+                ACustomer.GdprRequest = Convert.ToBoolean(DB.DataTable.Rows[Index]["GdprRequest"]);
+                mCustomerList.Add(ACustomer);
+                Index++;
+            }
+        }
+    }
+}
---
+++ Testing1/tstCustomerCollection.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace TestingCustomer
+{
+
+    [TestClass]
+    public class tstCustomerCollection
+    {
+        List<clsCustomer> CustomerList = new List<clsCustomer>();
+        Int32 Count = 0;
+        clsCustomer ThisCustomer = new clsCustomer();
+
+
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            Assert.IsNotNull(AllCustomers);
+        }
+
+        [TestMethod]
+        public void CustomerListOK()
+        {
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            List<clsCustomer> CustomerList = new List<clsCustomer>();
+            //create the test customer
+            clsCustomer TestCustomer = new clsCustomer();
+            //set properties
+            TestCustomer.CustomerId = 1;
+            TestCustomer.Name = "John Snow";
+            TestCustomer.Address = "1 Kings Landing";
+            TestCustomer.Postcode = "KL01 1QL";
+            TestCustomer.DoB = DateTime.Now.Date;
+            TestCustomer.GdprRequest = false;
+            //add TestCustomer to the CustomerList
+            CustomerList.Add(TestCustomer);
+            AllCustomers.CustomerList = CustomerList;
+            Assert.AreEqual(AllCustomers.CustomerList, CustomerList);
+        }
+
+        [TestMethod]
+        public void ThisCustomerPropertyOK()
+        {
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            //create the test customer
+            clsCustomer TestCustomer = new clsCustomer();
+            //set properties
+            TestCustomer.CustomerId = 10;
+            TestCustomer.Name = "Ten";
+            TestCustomer.Address = "Ten";
+            TestCustomer.Postcode = "Post";
+            TestCustomer.DoB = DateTime.Now.Date;
+            TestCustomer.GdprRequest = true;
+            AllCustomers.ThisCustomer = TestCustomer;
+            Assert.AreEqual(AllCustomers.ThisCustomer, TestCustomer);
+        }
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            List<clsCustomer> CustomerList = new List<clsCustomer>();
+            //create the test customer
+            clsCustomer TestCustomer = new clsCustomer();
+            //set properties
+            TestCustomer.CustomerID = 1;
+            TestCustomer.Name = "name";
+            TestCustomer.Address = "address";
+            TestCustomer.PhoneNumber = "1234";
+            TestCustomer.DateAdded = DateTime.Now.Date;
+            TestCustomer.Querry = true;
+            //add TestCustomer to the CustomerList
+            CustomerList.Add(TestCustomer);
+            AllCustomers.CustomerList = CustomerList;
+            Assert.AreEqual(AllCustomers.Count, CustomerList.Count);
+        }
+
+        [TestMethod]
+        public void AddMethodOK()
+        {
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            //create the test customer
+            clsCustomer TestCustomer = new clsCustomer();
+            Int32 PrimaryKey = 0;
+            //set properties
+            TestCustomer.CustomerID = 1;
+            TestCustomer.Name = "Clark Kent";
+            TestCustomer.Address = "9 Henshaw street";
+            TestCustomer.PhoneNumber = "1239387";
+            TestCustomer.DateAdded = DateTime.Now.Date;
+            TestCustomer.Querry = true;
+            AllCustomers.ThisCustomer = TestCustomer;
+            //add customer to key
+            PrimaryKey = AllCustomers.Add();
+            //set key
+            TestCustomer.CustomerID = PrimaryKey;
+            AllCustomers.ThisCustomer.Find(PrimaryKey);
+            Assert.AreEqual(AllCustomers.ThisCustomer, TestCustomer);
+        }
+
+        [TestMethod]
+        public void UpdateMethodOK()
+        {
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            //create the test customer
+            clsCustomer TestCustomer = new clsCustomer();
+            Int32 PrimaryKey = 0;
+            //set properties            
+            TestCustomer.Name = "name";
+            TestCustomer.Address = "address";
+            TestCustomer.PhoneNumber = "1234";
+            TestCustomer.DateAdded = DateTime.Now.Date;
+            TestCustomer.Querry = true;
+            AllCustomers.ThisCustomer = TestCustomer;
+            //add customer to key
+            PrimaryKey = AllCustomers.Add();
+            //set key
+            TestCustomer.CustomerID = PrimaryKey;
+            //update test customer
+            TestCustomer.Name = "Bruce Wayne";
+            TestCustomer.Address = "10 Deacon Street";
+            TestCustomer.PhoneNumber = "12223344";
+            TestCustomer.DateAdded = DateTime.Now.Date;
+            TestCustomer.Querry = false;
+            //set customer with new data
+            AllCustomers.ThisCustomer = TestCustomer;
+            AllCustomers.Update();
+            AllCustomers.ThisCustomer.Find(PrimaryKey);
+            Assert.AreEqual(AllCustomers.ThisCustomer, TestCustomer);
+        }
+
+        [TestMethod]
+        public void DeleteMethodOK()
+        {
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            //create the test customer
+            clsCustomer TestCustomer = new clsCustomer();
+            Int32 PrimaryKey = 0;
+            //set properties
+            TestCustomer.Name = "name";
+            TestCustomer.Address = "address";
+            TestCustomer.PhoneNumber = "1234";
+            TestCustomer.DateAdded = DateTime.Now.Date;
+            TestCustomer.Querry = true;
+            AllCustomers.ThisCustomer = TestCustomer;
+            //add customer to key
+            PrimaryKey = AllCustomers.Add();
+            TestCustomer.CustomerID = PrimaryKey;
+            AllCustomers.ThisCustomer.Find(PrimaryKey);
+            AllCustomers.Delete();
+            Boolean found = AllCustomers.ThisCustomer.Find(PrimaryKey);
+            Assert.IsFalse(found);
+        }
+        [TestMethod]
+        public void ReportByNameOK()
+        {
+            //normal instance
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            //filtered data instance
+            clsCustomerCollection FilteredNames = new clsCustomerCollection();
+            //empty filtered name
+            FilteredNames.ReportByName("");
+            Assert.AreEqual(AllCustomers.Count, FilteredNames.Count);
+        }
+
+        [TestMethod]
+        public void ReportByNameNoneFound()
+        {
+            //filtered data instance
+            clsCustomerCollection FilteredNames = new clsCustomerCollection();
+            //apply a name that does not exist
+            FilteredNames.ReportByName("abcdefg");
+            //test to see that there are no records
+            Assert.AreEqual(0, FilteredNames.Count);
+        }
+
+        [TestMethod]
+        public void ReportByNameDataFound()
+        {
+            //filtered data instance
+            clsCustomerCollection FilteredNames = new clsCustomerCollection();
+            Boolean OK = true;
+            //apply a name from the list
+            FilteredNames.ReportByName("Peter Parker");
+            if (FilteredNames.Count == 2)
+            {
+                //ID of customer is 74
+                if (FilteredNames.CustomerList[0].CustomerID != 74)
+                {
+                    OK = false;
+                }
+
+                //ID of customer is 75 
+                if (FilteredNames.CustomerList[1].CustomerID != 75)
+                {
+                    OK = false;
+                }
+            }
+            else
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+    }
+}
